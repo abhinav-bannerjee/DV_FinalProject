bf135a9f88e644581b09ba308422f6bfddc89f1b
Ke Chen <CK@wireless-10-146-150-133.public.utexas.edu>
2015-12-06 17:22:18 -0600

    data

--- 01 Data/1_etl.R
+++ 01 Data/1_etl.R
@@ -8,29 +8,11 @@ df <- read.csv(file_path)
 # Replace "." (i.e., period) with "_" in the column names.
 names(df) <- gsub("\\.+", "_", names(df))
 
-#str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
-names(df)[7] <- "Sales"
-names(df)[8] <-"Profits"
-  names(df)[9] <-"Assets"
-  names(df)[10] <-"Market_Value"
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
 measures <- c("Rank","Year","Sales","Profits","Assets","Market_Value")
 
-dimensions <- setdiff(names(df), measures)
 
 
-if( length(measures) > 1 || ! is.na(dimensions)) {
-  for(d in dimensions) {
-    # Get rid of " and ' in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern="[\"']",replacement= ""))
-    # Change & to and in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern="&",replacement= " and "))
-    # Change : to ; in dimensions.
-    df[d] <- data.frame(lapply(df[d], gsub, pattern=":",replacement= ";"))
-  }
-}
-
-
-library(lubridate)
 
 
 write.csv(format(df, scientific = NA), paste(gsub(".csv", "", file_path), "..csv", sep="" ), row.names=FALSE, na = "")
