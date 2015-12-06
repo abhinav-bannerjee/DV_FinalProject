51b29afd9bfced12ec9a32e839d819e401ccf124
Ke Chen <CK@wireless-10-146-150-133.public.utexas.edu>
2015-12-06 17:24:39 -0600

    DATAFRAME

---
+++ 01 Data/DATAFRAME1.R
@@ -0,0 +1,4 @@
+require("jsonlite")
+require("RCurl")
+# Change the USER and PASS below to be your UTEid
+df1 <- data.frame(fromJSON(getURL(URLencode('skipper.cs.utexas.edu:5001/rest/native/?query="select * from TOPCOMPANY "'),httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_kc35827', PASS='orcl_kc35827', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE) ))
