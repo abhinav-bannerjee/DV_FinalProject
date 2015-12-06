ba33d7b6418a17f5d3f47d28e50684864b6cdb4c
Ke Chen <CK@wireless-10-146-150-133.public.utexas.edu>
2015-12-06 17:23:02 -0600

    data

---
+++ 01 Data/2_etl.R
@@ -0,0 +1,33 @@
+setwd("/Users/CK/DataVisualization/DV_Finalproject/01 Data")
+
+file_path <- "2.csv"
+
+df <- read.csv(file_path)
+
+
+# Replace "." (i.e., period) with "_" in the column names.
+names(df) <- gsub("\\.+", "_", names(df))
+
+str(df) # Uncomment this and  run just the lines to here to get column types to use for getting the list of measures.
+measures <- c()
+dimensions <- setdiff(names(df), measures)
+
+
+
+write.csv(format(df, scientific = NA), paste(gsub(".csv", "", file_path), "..csv", sep="" ), row.names=FALSE, na = "")
+
+tableName <- gsub(" +", "_", gsub("[^A-z, 0-9, ]", "", gsub(".csv", "", file_path)))
+sql <- paste("CREATE TABLE", tableName, "(\n-- Change table_name to the table name you want.\n")
+if( length(measures) > 1 || ! is.na(dimensions)) {
+  for(d in dimensions) {
+    sql <- paste(sql, paste(d, "varchar2(4000),\n"))
+  }
+}
+if( length(measures) > 1 || ! is.na(measures)) {
+  for(m in measures) {
+    if(m != tail(measures, n=1)) sql <- paste(sql, paste(m, "number(38,4),\n"))
+    else sql <- paste(sql, paste(m, "number(38,4)\n"))
+  }
+}
+sql <- paste(sql, ");")
+cat(sql)
