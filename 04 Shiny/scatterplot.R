1d508c8b5a89e606c4bae203428d24efaf849e82
Ke Chen <CK@wireless-10-146-106-143.public.utexas.edu>
2015-12-07 20:00:08 -0600

    red

--- 04 Shiny/scatterplot.R
+++ 04 Shiny/scatterplot.R
@@ -1,4 +1,4 @@
-df <- df%>% arrange(desc(Profits))
+df1 <- df1%>% arrange(desc(Profits))
 ggplot() + 
   scale_colour_gradientn(name = "Profits",colours = rainbow(15, start =0, end = 0.99))+
   coord_cartesian() + 
@@ -6,15 +6,15 @@ ggplot() +
   scale_y_continuous() +
   labs(title='RANKS') +
   labs(x="Rank", y=paste("Profit")) +
-
-  layer(data=df, 
-        mapping=aes(x=Rank, y=Profits,color = as.numeric(Profits)),
+  
+  layer(data=df1, 
+        mapping=aes(x=RANK, y=PROFITS,color = as.numeric(PROFITS)),
         stat="identity", 
         stat_params=list(), 
         geom="point",
         geom_params=list(), 
         position=position_jitter(width=0.3, height=0)
-  )+layer(data=slice(df, c(1:10, 20,30,50,80,100,150,200,250,350,450,433,
+  )+layer(data=slice(df1, c(1:10, 20,30,50,80,100,150,200,250,350,450,433,
                            533,
                            633,
                            733,
@@ -31,13 +31,12 @@ ggplot() +
                            1833,
                            1933,
                            2033,
-                           2133)), 
-          mapping=aes(x=Rank, y=Profits,label=Profits), 
+                           2133, recursive = FALSE)), 
+          mapping=aes(x=RANK, y=PROFITS,label=PROFITS), 
           stat="identity", 
           stat_params=list(), 
           geom="text",
           geom_params=list(colour="red", hjust=-0.3), 
           position=position_identity()
   )+
-  stat_smooth(data = df,mapping = aes(x=Rank, y=Profits),geom = "smooth", position = "identity",method = "lm",formula= y~log(x))
-                
+  stat_smooth(data = df1,mapping = aes(x=RANK, y=PROFITS),geom = "smooth", position = "identity",method = "lm",formula= y~log(x))
